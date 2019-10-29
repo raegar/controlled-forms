282b1e9d491595fecf7ec49908142a4fcd0a8451
Jamie Myland <jamie.myland@gmail.com>
2019-10-29 16:46:03 +0000

    Added Form and Field Components using a Reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React, { useEffect, useState  } from "react";
+import React, { useEffect, useReducer } from "react";
+
 import "./App.css";
+import { Form, Field, AppFormActions } from './Form';
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  gender: "",
+  email: ""
+};
 
 const App = () => {
-  const [value, setValue] = useState('');
-
-  useEffect(() => console.log(value), [value]);
-
-  const handleChange = event => setValue(event.target.value);
-  
   return (
     <div className="App">
-      <label>
-        My Controlled Input: 
-        <input type="text" value={value} onChange={handleChange} />
-      </label>
-
-      <p>
-        <strong>Output:</strong> {value}
-      </p>
+      <Form
+        initialValues={{
+          firstName: "FirstName",
+          lastName: "LastName",
+          age: 0,
+          gender: "Select",
+          email: "Email"
+        }}
+        validate={values => {
+          if (!values.firstName) {
+            return { firstName: "First name is required." };
+          }
+        }}
+        onSubmit={values => alert(JSON.stringify(values))}
+      >
+        <Field name="firstName" />
+        <Field name="lastName" />
+        <Field name="age" type="number" />
+        <Field name="gender" component="select">
+          <option>Male</option>
+          <option>Female</option>
+          <option>Other</option>
+        </Field>
+        <Field name="email" />
+        <AppFormActions />
+      </Form>
     </div>
   );
 };
---
+++ src/Form.js
@@ -0,0 +1,105 @@
+import React, { useReducer, useEffect, useContext } from "react";
+function defaultReducer(state, action) {
+    switch (action.type) {
+      case "reset":
+        return { ...action.payload };
+      case "set":
+        return { ...state, [action.name]: action.value };
+      case "setValues":
+        return { ...action.payload };
+      default:
+        return state;
+    }
+  }
+  
+  const FormContext = React.createContext({});
+  
+  export function Form(props) {
+    const {
+      initialValues = {},
+      validate = () => null,
+      initialErrors = {},
+      reducer = defaultReducer,
+      onSubmit = () => null,
+      children
+    } = props;
+    const [values, dispatch] = useReducer(reducer, initialValues);
+    const [errors, errorDispatch] = useReducer(reducer, initialErrors);
+    const formProps = {
+      values,
+      errors,
+      runValidations: async values => {
+        const validateErrors = await validate(values);
+        console.log(
+          "runValidations",
+          JSON.stringify(values),
+          JSON.stringify(validateErrors)
+        );
+        errorDispatch({ type: "setValues", payload: validateErrors });
+      },
+      setFieldValue: function(name, value) {
+        dispatch({ type: "set", name, value });
+        //TODO this has to be run after the dispatch is successful
+        // formProps.runValidations({ ...values, [name]: value });
+      },
+      handleSubmit: e => {
+        e.preventDefault();
+        onSubmit(values);
+      },
+      handleReset: e => {
+        dispatch({ type: "reset", payload: initialValues });
+      }
+    };
+  
+    useEffect(
+      () => {
+        formProps.runValidations(values);
+      },
+      [values]
+    );
+  
+    return (
+      <FormContext.Provider value={formProps}>
+        <form onSubmit={formProps.handleSubmit}>{children}</form>
+      </FormContext.Provider>
+    );
+  }
+  
+  export function Field(props) {
+    const { name, component, ...otherProps } = props;
+    const { values, setFieldValue } = useContext(FormContext);
+  
+    const Component = component || "input";
+  
+    return React.createElement(Component, {
+      type: "text",
+      value: values[name],
+      onChange: e => setFieldValue(name, e.target.value),
+      ...otherProps
+    });
+  }
+
+  export const AppFormActions = () => {
+    const { values, errors, handleSubmit, handleReset } = useContext(FormContext);
+  
+    return (
+      <>
+        <div>
+          <button type="submit">Save</button>
+          <button type="button" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+        <div>
+          Values:
+          <pre>{JSON.stringify(values, null, 2)}</pre>
+        </div>
+        <div>
+          Errors:
+          <pre>{JSON.stringify(errors, null, 2)}</pre>
+        </div>
+      </>
+    );
+  }
+
+export default {Form, Field, AppFormActions};
